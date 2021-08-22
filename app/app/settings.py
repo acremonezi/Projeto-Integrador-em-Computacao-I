776c1c1cf3d20551894ef6b78448ec94e6081db4
Alcides <acremonezi@gmail.com>
2021-08-21 23:47:06 -0300

    django mysql setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql_pi1',
+        'USER': 'mysql_pi1',
+        'PASSWORD': 'F6fmy9l1SzRcD1Zqsxhs',
+        'HOST': '127.0.0.1',
+        'PORT': '33061',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
