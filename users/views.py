ac23fee7b3def406f072ee07cb2d737fcca37d27
Alcides <acremonezi@gmail.com>
2021-10-04 21:04:21 -0300

    geral

--- users/views.py
+++ users/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# SignUP
 def signup(request):
-    return HttpResponse('funcao signup')
+    # return HttpResponse('funcao signup') // teste view e urls 
+    status = request.GET.get('status')
+    return render(request, 'signup.html', {'status': status})
 
+# SignIN
 def signin(request):
-    return HttpResponse('funcao signin')
+    # return HttpResponse('funcao signin') // teste view e urls 
+    status = request.GET.get('status')
+    return render(request, 'signin.html', {'status': status})
 
